b4ed6e34a0b7f640b07f95d2061e6951fee83a68
Peter Schumacher <pschu@me.com>
2016-02-07 00:17:10 +0100

    only changing Usergrid.overrideUrl if USERGRID_HOST is set

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 if [ -z "${USERGRID_HOST}" ]; then
-  # ip and port of the usergrid api
-  USERGRID_HOST=192.168.1.34:8080
+else 
+	# patch url where the portal can find the usergrid api
+	sed -i "s#Usergrid.overrideUrl = 'http://localhost:8080/';#Usergrid.overrideUrl = 'http://${USERGRID_HOST}/';#g" /var/www/html/config.js
 fi
 
-# patch url where the portal can find the usergrid api
-sed -i "s#Usergrid.overrideUrl = 'http://localhost:8080/';#Usergrid.overrideUrl = 'http://${USERGRID_HOST}/';#g" /var/www/html/config.js
+
 
 service nginx start
 tail -f /var/log/nginx/error.log /var/log/nginx/access.log

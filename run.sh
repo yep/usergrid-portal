efe0204787f0c1fd79cbef1aad30444c8a005c2a
yep <yep171@yahoo.com>
2016-02-07 14:28:23 +0100

    only update config if usergrid host variable is set

--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-if [ -z "${USERGRID_HOST}" ]; then
-  # ip and port of the usergrid api
-  USERGRID_HOST=192.168.1.34:8080
+if [ -n "${USERGRID_HOST}" ]; then
+  # patch url where the portal can find the usergrid api if USERGRID_HOST is set
+  sed -i "s#Usergrid.overrideUrl = 'http://localhost:8080/';#Usergrid.overrideUrl = 'http://${USERGRID_HOST}/';#g" /var/www/html/config.js
 fi
-
-# patch url where the portal can find the usergrid api
-sed -i "s#Usergrid.overrideUrl = 'http://localhost:8080/';#Usergrid.overrideUrl = 'http://${USERGRID_HOST}/';#g" /var/www/html/config.js
 
 service nginx start
 tail -f /var/log/nginx/error.log /var/log/nginx/access.log
